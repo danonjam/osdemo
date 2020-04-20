a216bb672891cbe4245fa91ddea20a068df84337
danonjam <33768802+danonjam@users.noreply.github.com>
2020-04-20 01:03:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome development team! update 3"
+    return "Welcome development team! update 4"
 
 @app.route('/test')
 def hello():
